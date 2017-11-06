c70a5c50fd88f4f2a7491e4c827e6e41c75d5695
marisaslozano <marisalozano1995@gmail.com>
2017-11-06 16:27:13 -0500

    put alpha as first arument

--- linearAlgebra/Week1/axpyTests.py
+++ linearAlgebra/Week1/axpyTests.py
@@ -13,78 +13,78 @@ class AxpyTests(unittest.TestCase):
         practice = Week1Functions()
         self.assertEqual(
             [-2, -5, -8],
-            practice.axpy(self.vector_x, self.vector_y, self.alpha))
+            practice.axpy( self.alpha, self.vector_x, self.vector_y))
 
     def test_column_row_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
             [-2, -5, -8],
             practice.axpy(
-                self.vector_x, np.transpose(self.vector_y), self.alpha))
+                 self.alpha, self.vector_x, np.transpose(self.vector_y)))
 
     def test_row_column_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
             [-2, -5, -8],
             practice.axpy(
-                np.transpose(self.vector_x), self.vector_y, self.alpha))
+                 self.alpha, np.transpose(self.vector_x), self.vector_y))
 
     def test_row_row_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
             [-2, -5, -8],
             practice.axpy(
-                np.transpose(
-                    self.vector_x), np.transpose(self.vector_y), self.alpha))
+                self.alpha, np.transpose(
+                    self.vector_x), np.transpose(self.vector_y)))
 
     def test_vector_x_wrong_size_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            'FAILED', practice.axpy([-1], self.vector_y, self.alpha))
+            'FAILED', practice.axpy(self.alpha, [-1], self.vector_y))
 
     def test_vector_y_wrong_size_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            'FAILED', practice.axpy(self.vector_x, [4, 3, 2, 1], self.alpha))
+            'FAILED', practice.axpy(self.alpha, self.vector_x, [4, 3, 2, 1]))
 
     def test_vector_x_zeroes_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            [0, -1, -2], practice.axpy([0, 0, 0], self.vector_y, self.alpha))
+            [0, -1, -2], practice.axpy(self.alpha, [0, 0, 0], self.vector_y))
 
     def test_vector_y_zeroes_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            [-2, -4, -6], practice.axpy(self.vector_x, [0, 0, 0], self.alpha))
+            [-2, -4, -6], practice.axpy(self.alpha, self.vector_x, [0, 0, 0]))
 
     def test_all_vectors_zeroes_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            [0, 0, 0], practice.axpy([0, 0, 0], [0, 0, 0], self.alpha))
+            [0, 0, 0], practice.axpy(self.alpha, [0, 0, 0], [0, 0, 0]))
 
     def test_vector_x_empty_and_wrong_size_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            'FAILED', practice.axpy([], self.vector_y, self.alpha))
+            'FAILED', practice.axpy(self.alpha, [], self.vector_y))
 
     def test_vector_y_empty_and_wrong_size_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            'FAILED', practice.axpy(self.vector_x, [], self.alpha))
+            'FAILED', practice.axpy(self.alpha, self.vector_x, []))
 
     def test_empty_vectors_axpy(self):
         practice = Week1Functions()
-        self.assertEqual([], practice.axpy([], [], self.alpha))
+        self.assertEqual([], practice.axpy(self.alpha, [], []))
 
     def test_zero_alpha_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            [0, -1, -2], practice.axpy(self.vector_x, self.vector_y, 0))
+            [0, -1, -2], practice.axpy(0, self.vector_x, self.vector_y))
 
     def test_false_alpha_axpy(self):
         practice = Week1Functions()
         self.assertEqual(
-            'FAILED', practice.axpy(self.vector_x, self.vector_y, None))
+            'FAILED', practice.axpy(None, self.vector_x, self.vector_y))
 
 if __name__ == '__main__':
     unittest.main()
