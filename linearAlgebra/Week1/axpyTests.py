ce3563a70f05d83cbbe68e665cec000526a0b80f
marisaslozano <marisalozano1995@gmail.com>
2017-11-07 15:57:24 -0500

    add unbounded/partition function of axpy

--- linearAlgebra/Week1/axpyTests.py
+++ linearAlgebra/Week1/axpyTests.py
@@ -86,5 +86,91 @@ class AxpyTests(unittest.TestCase):
         self.assertEqual(
             'FAILED', practice.axpy(None, self.vector_x, self.vector_y))
 
+
+class AxpyUnbTests(unittest.TestCase):
+    def setUp(self):
+        self.vector_x = [1, 2, 3]
+        self.vector_y = [0, -1, -2]
+        self.alpha = -2
+
+    def test_column_column_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [-2, -5, -8],
+            practice.axpy_unb(self.alpha, self.vector_x, self.vector_y))
+
+    def test_column_row_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [-2, -5, -8],
+            practice.axpy_unb(
+                 self.alpha, self.vector_x, np.transpose(self.vector_y)))
+
+    def test_row_column_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [-2, -5, -8],
+            practice.axpy_unb(
+                 self.alpha, np.transpose(self.vector_x), self.vector_y))
+
+    def test_row_row_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [-2, -5, -8],
+            practice.axpy_unb(
+                self.alpha, np.transpose(
+                    self.vector_x), np.transpose(self.vector_y)))
+
+    def test_vector_x_wrong_size_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            'FAILED', practice.axpy_unb(self.alpha, [-1], self.vector_y))
+
+    def test_vector_y_wrong_size_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            'FAILED', practice.axpy_unb(self.alpha, self.vector_x, [4, 3, 2, 1]))
+
+    def test_vector_x_zeroes_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [0, -1, -2],
+            practice.axpy_unb(self.alpha, [0, 0, 0], self.vector_y))
+
+    def test_vector_y_zeroes_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [-2, -4, -6],
+            practice.axpy_unb(self.alpha, self.vector_x, [0, 0, 0]))
+
+    def test_all_vectors_zeroes_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [0, 0, 0], practice.axpy_unb(self.alpha, [0, 0, 0], [0, 0, 0]))
+
+    def test_vector_x_empty_and_wrong_size_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            'FAILED', practice.axpy_unb(self.alpha, [], self.vector_y))
+
+    def test_vector_y_empty_and_wrong_size_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            'FAILED', practice.axpy_unb(self.alpha, self.vector_x, []))
+
+    def test_empty_vectors_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual([], practice.axpy_unb(self.alpha, [], []))
+
+    def test_zero_alpha_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            [0, -1, -2], practice.axpy_unb(0, self.vector_x, self.vector_y))
+
+    def test_false_alpha_axpy_unb(self):
+        practice = Week1Functions()
+        self.assertEqual(
+            'FAILED', practice.axpy_unb(None, self.vector_x, self.vector_y))
+
 if __name__ == '__main__':
     unittest.main()
