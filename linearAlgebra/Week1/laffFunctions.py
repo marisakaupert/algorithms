43fbc108de4cc7409a0035ace17dcf48a4c9888c
marisaslozano <marisalozano1995@gmail.com>
2017-11-07 15:58:06 -0500

    add 2 functions: unbounded axpy and unbounded dot product

--- linearAlgebra/Week1/laffFunctions.py
+++ linearAlgebra/Week1/laffFunctions.py
@@ -14,6 +14,25 @@ class Week1Functions(object):
         y_out = list(np.add(scaled_x, vector_y))
         return y_out
 
+    def axpy_unb(self, alpha, vector_x, vector_y):
+        if len(vector_x) != len(vector_y):
+            return 'FAILED'
+
+        if alpha is None or isinstance(alpha, str):
+            return 'FAILED'
+
+        x_top = []
+        vector_x = list(vector_x)
+        vector_y = list(vector_y)
+        while len(x_top) < len(vector_x):
+            for index, val in enumerate(vector_x):
+                out_axpy = alpha * val + vector_y[index]
+                x_top.append(out_axpy)
+            vector_x.remove(val)
+
+        y_out = x_top
+        return y_out
+
     def copy(self, vector_x, vector_y):
         if vector_x is None or vector_y is None:
             return 'FAILED'
@@ -32,6 +51,23 @@ class Week1Functions(object):
         multiplied_vectors = np.multiply(vector_x, vector_y)
         alpha = int(np.sum(multiplied_vectors) + alpha)
         return alpha
+
+    def dot_product_unb(self, vector_x, vector_y):
+        if len(vector_x) != len(vector_y):
+            return 'FAILED'
+
+        x_top = []
+        alpha = 0
+        vector_x = list(vector_x)
+        vector_y = list(vector_y)
+        while len(x_top) < len(vector_x):
+            for index, val in enumerate(vector_x):
+                alpha = val * vector_y[index] + alpha
+                x_top.append(alpha)
+            vector_x.remove(val)
+
+        alpha_out = alpha
+        return alpha_out
 
     def length(self, vector_x):
         if vector_x is None:
