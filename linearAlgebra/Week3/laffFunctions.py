803550de45e1a232d98bd717ead1847ac156269f
marisaslozano <marisalozano1995@gmail.com>
2017-11-07 16:01:33 -0500

    add zero matrix function

--- linearAlgebra/Week3/laffFunctions.py
+++ linearAlgebra/Week3/laffFunctions.py
@@ -14,3 +14,21 @@ class Week3Functions(object):
                 x_out.append(0)
 
         return x_out
+
+    def set_to_zero_unb(self, matrix_A):
+        if matrix_A is None:
+            return 'FAILED'
+
+        A_left = []
+        while len(A_left) < len(matrix_A):
+            pivot = 0
+            current_vector = []
+            for item in np.nditer(matrix_A[pivot], order='F'):
+                item = 0
+                current_vector.append(item)
+                np.delete(matrix_A, item)
+            A_left.append(current_vector)
+            pivot = pivot + 1
+
+        zero_matrix = A_left
+        return zero_matrix
--- linearAlgebra/Week3/setToZeroTests.py
+++ linearAlgebra/Week3/setToZeroTests.py
@@ -1,8 +1,9 @@
 import unittest
+import numpy as np
 from laffFunctions import Week3Functions
 
 
-class ScaleTests(unittest.TestCase):
+class SetToZeroTests(unittest.TestCase):
     def setUp(self):
         self.vector_x = [1, 2, 3]
 
@@ -25,6 +26,30 @@ class ScaleTests(unittest.TestCase):
     def test_string_vector_set_to_zero_function(self):
         practice = Week3Functions()
         self.assertEqual('FAILED', practice.setToZero(['a', 'b', 'c']))
+
+
+class SetToZeroUnblockedTests(unittest.TestCase):
+    def setUp(self):
+        self.matrix_A = np.matrix('1 2 3; 0 -1 -2')
+        self.zero_matrix = np.matrix('0 0; 0 0')
+
+    def test_set_to_zero_function_unb(self):
+        practice = Week3Functions()
+        self.assertEqual(
+            [[0, 0, 0], [0, 0, 0]], practice.set_to_zero_unb(self.matrix_A))
+
+    def test_already_zero_vector_set_to_zero_function_unb(self):
+        practice = Week3Functions()
+        self.assertEqual(
+            [[0, 0], [0, 0]], practice.set_to_zero_unb(self.zero_matrix))
+
+    def test_empty_vector_set_to_zero_function_unb(self):
+        practice = Week3Functions()
+        self.assertEqual([], practice.set_to_zero_unb([]))
+
+    def test_illegal_vector_set_to_zero_function_unb(self):
+        practice = Week3Functions()
+        self.assertEqual('FAILED', practice.set_to_zero_unb(None))
 
 
 if __name__ == '__main__':
