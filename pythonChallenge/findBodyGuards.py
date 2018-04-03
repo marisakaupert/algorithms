92f3ec2be940ce30464ad379d7cb9934dfba901a
marisaslozano <marisalozano1995@gmail.com>
2018-04-03 00:09:38 -0800

    complete challenge to parse through bodyguard letters

--- pythonChallenge/findBodyGuards.py
+++ pythonChallenge/findBodyGuards.py
@@ -1,2 +1,19 @@
-def findBodyGuards():
-    pass
+import re
+
+
+def findBodyGuards(text_file):
+    result_string = ""
+    pattern = r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]'
+    with open(text_file, "r") as lines:
+        for line in lines:
+            search = re.findall(pattern, line)
+            for letter in search:
+                result_string = result_string + letter
+
+    print(result_string)
+
+
+findBodyGuards("pageSource2.txt")
+
+# Next url:
+# http://www.pythonchallenge.com/pc/def/linkedlist.php
